8648d037341d1fb70049fae6d762f632c9c7bd79
Danendra Satriyo Buwono <xxdanen@gmail.com>
2024-06-15 00:18:01 +0700

    Membuat fitur log out

--- app/src/main/java/org/d3if3007/assessment3/ui/screen/ProfilDialog.kt
+++ app/src/main/java/org/d3if3007/assessment3/ui/screen/ProfilDialog.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -85,12 +86,21 @@ fun ProfilDialog(
                     ) {
                         Text(text = stringResource(id = R.string.tutup))
                     }
+                    OutlinedButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = stringResource(id = R.string.log_out),
+                            color = MaterialTheme.colorScheme.error
+                        )
+                    }
+                }
                 }
             }
 
         }
     }
-}
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)

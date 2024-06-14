82c4aa03a6ad39cb309672065411fa32ab5aca07
Danendra Satriyo Buwono <xxdanen@gmail.com>
2024-06-15 00:14:58 +0700

    Membuat tampilan profil

--- app/src/main/java/org/d3if3007/assessment3/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3007/assessment3/ui/screen/MainScreen.kt
@@ -3,22 +3,9 @@ package org.d3if3007.assessment3.ui.screen
 import android.content.Context
 import android.content.res.Configuration
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Text
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -30,26 +17,20 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
 import androidx.credentials.exceptions.GetCredentialException
-import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
@@ -61,6 +42,7 @@ import org.d3if3007.assessment3.R
 import org.d3if3007.assessment3.model.User
 import org.d3if3007.assessment3.network.UserDataStore
 import org.d3if3007.assessment3.ui.theme.Assessment3Theme
+
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
@@ -68,9 +50,13 @@ fun MainScreen() {
     val dataStore = UserDataStore(context)
     val user by dataStore.userFlow.collectAsState(initial = User())
 
+    var showDialog by remember { mutableStateOf(false) }
+
+
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = stringResource(id = R.string.app_name))
+            TopAppBar(title = {
+                Text(text = stringResource(id = R.string.app_name))
             },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -78,14 +64,15 @@ fun MainScreen() {
                 ),
                 actions = {
                     IconButton(onClick = {
-                        if (user.email.isEmpty()){
+                        if (user.email.isEmpty()) {
                             CoroutineScope(Dispatchers.IO).launch { signIn(context, dataStore) }
-                        }
-                        else {
-                            Log.d("SIGN-IN", "User: $user")
+                        } else {
+                            showDialog = true
+
                         }
 
-                    }) {                        Icon(
+                    }) {
+                        Icon(
                             painter = painterResource(id = R.drawable.account_circle),
                             contentDescription = stringResource(id = R.string.profil),
                             tint = MaterialTheme.colorScheme.primary
@@ -95,19 +82,27 @@ fun MainScreen() {
                 }
             )
         }
-    ) {padding ->
+    ) { padding ->
         ScreenContent(Modifier.padding(padding))
+
+        if (showDialog){
+            ProfilDialog(
+                user = user,
+                onDismissRequest = { showDialog = false }) {
+                showDialog = false
+            }
+        }
     }
 }
 
 
 @Composable
-fun ScreenContent(modifier: Modifier){
-    Column (
+fun ScreenContent(modifier: Modifier) {
+    Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
-    ){
+    ) {
 
     }
 }
@@ -139,10 +134,11 @@ private suspend fun handleSignIn(result: GetCredentialResponse, dataStore: UserD
     ) {
         try {
             val googleId = GoogleIdTokenCredential.createFrom(credential.data)
-            val name = googleId.displayName?: ""
+            val name = googleId.displayName ?: ""
             val email = googleId.id
             val photoUrl = googleId.profilePictureUri.toString()
-            dataStore.saveData(User(name,email, photoUrl))        } catch (e: GoogleIdTokenParsingException) {
+            dataStore.saveData(User(name, email, photoUrl))
+        } catch (e: GoogleIdTokenParsingException) {
             Log.e("SIGN-IN", "Error: ${e.message}")
         }
     } else {
@@ -154,7 +150,7 @@ private suspend fun handleSignIn(result: GetCredentialResponse, dataStore: UserD
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun ScreenPreview() {
-   Assessment3Theme {
+    Assessment3Theme {
         MainScreen()
     }
 }
